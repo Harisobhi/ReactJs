7d6880cdcff146f32a9bfc7f9b92b41ca2042b98
iyynHari <harishankar@iyyntechnology.com>
2024-08-29 12:08:27 +0530

    Changes

---
+++ firstApp/src/Example.jsx
@@ -0,0 +1,11 @@
+import styles from './Example.module.css';
+
+function Example() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Hello, CSS Modules!</h1>
+    </div>
+  );
+}      
+
+export default Example;
