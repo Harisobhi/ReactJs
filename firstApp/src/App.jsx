b4c5a3a4a763f656b3b91cefb207a8b59a929504
iyynHari <harishankar@iyyntechnology.com>
2024-08-29 12:38:34 +0530

    Router

--- firstApp/src/App.jsx
+++ firstApp/src/App.jsx
@@ -1,21 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import Sample from './Sample.jsx';
-// import Footer from './Footer.jsx';
-// import Header from './Header.jsx';
-import Main from './Index.jsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
 import './index.css';
+import Main from './Index.jsx';
 import App1 from './App1.jsx';
 import Example from './Example.jsx';
+import Connect from './Connect.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Header />
+        {/* <Header />
     <Sample />
     <Footer /> */}
+
     <Main />
     <App1 />
     <Example />
-    
-  </StrictMode>,
-)
+    <App1 />
+    <Connect />
+
+  </StrictMode>
+);
+
+export default App;
---
+++ firstApp/src/Connect.jsx
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+
+function Connect() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default Connect;
